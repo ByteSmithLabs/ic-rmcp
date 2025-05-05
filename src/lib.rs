c74156f4756fb2e82d1929c9995dd425ba6710b0
kyle <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-05 16:08:29 +0700

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,4 @@
-mod model;
+mod error;
+pub use error::Error;
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(add(2, 2), 4);
-    }
-}
+pub mod model;
