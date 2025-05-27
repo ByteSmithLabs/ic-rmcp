126dc7ec1747e0f59b78350e89eaba55ef90f648
kyle <92897177+okoyfoeciov@users.noreply.github.com>
2025-05-27 20:42:48 +0700

    update

--- src/handler.rs
+++ src/handler.rs
@@ -9,40 +9,6 @@ impl<H: ServerHandler> Service for H {
                 .await
                 .map(ServerResult::InitializeResult),
             ClientRequest::PingRequest(_request) => self.ping().await.map(ServerResult::empty),
-            ClientRequest::CompleteRequest(request) => self
-                .complete(request.params)
-                .await
-                .map(ServerResult::CompleteResult),
-            ClientRequest::SetLevelRequest(_) => {
-                Err(Error::method_not_found::<SetLevelRequestMethod>())
-                    .map(ServerResult::CompleteResult)
-            }
-            ClientRequest::GetPromptRequest(request) => self
-                .get_prompt(request.params)
-                .await
-                .map(ServerResult::GetPromptResult),
-            ClientRequest::ListPromptsRequest(request) => self
-                .list_prompts(request.params)
-                .await
-                .map(ServerResult::ListPromptsResult),
-            ClientRequest::ListResourcesRequest(request) => self
-                .list_resources(request.params)
-                .await
-                .map(ServerResult::ListResourcesResult),
-            ClientRequest::ListResourceTemplatesRequest(request) => self
-                .list_resource_templates(request.params)
-                .await
-                .map(ServerResult::ListResourceTemplatesResult),
-            ClientRequest::ReadResourceRequest(request) => self
-                .read_resource(request.params)
-                .await
-                .map(ServerResult::ReadResourceResult),
-            ClientRequest::SubscribeRequest(_) => {
-                Err(Error::method_not_found::<SubscribeRequestMethod>()).map(ServerResult::empty)
-            }
-            ClientRequest::UnsubscribeRequest(_) => {
-                Err(Error::method_not_found::<UnsubscribeRequestMethod>()).map(ServerResult::empty)
-            }
             ClientRequest::CallToolRequest(request) => self
                 .call_tool(request.params)
                 .await
@@ -51,6 +17,7 @@ impl<H: ServerHandler> Service for H {
                 .list_tools(request.params)
                 .await
                 .map(ServerResult::ListToolsResult),
+            _ => Err(Error::new(ErrorCode::METHOD_NOT_FOUND,"Method not found", None)),
         }
     }
 
@@ -93,42 +60,6 @@ pub trait ServerHandler {
 
         info.protocol_version = negotiated_protocol_version;
         std::future::ready(Ok(info))
-    }
-    fn complete(
-        &self,
-        request: CompleteRequestParam,
-    ) -> impl Future<Output = Result<CompleteResult, Error>> {
-        std::future::ready(Err(Error::method_not_found::<CompleteRequestMethod>()))
-    }
-    fn get_prompt(
-        &self,
-        request: GetPromptRequestParam,
-    ) -> impl Future<Output = Result<GetPromptResult, Error>> {
-        std::future::ready(Err(Error::method_not_found::<GetPromptRequestMethod>()))
-    }
-    fn list_prompts(
-        &self,
-        request: Option<PaginatedRequestParam>,
-    ) -> impl Future<Output = Result<ListPromptsResult, Error>> {
-        std::future::ready(Ok(ListPromptsResult::default()))
-    }
-    fn list_resources(
-        &self,
-        request: Option<PaginatedRequestParam>,
-    ) -> impl Future<Output = Result<ListResourcesResult, Error>> {
-        std::future::ready(Ok(ListResourcesResult::default()))
-    }
-    fn list_resource_templates(
-        &self,
-        request: Option<PaginatedRequestParam>,
-    ) -> impl Future<Output = Result<ListResourceTemplatesResult, Error>> {
-        std::future::ready(Ok(ListResourceTemplatesResult::default()))
-    }
-    fn read_resource(
-        &self,
-        request: ReadResourceRequestParam,
-    ) -> impl Future<Output = Result<ReadResourceResult, Error>> {
-        std::future::ready(Err(Error::method_not_found::<ReadResourceRequestMethod>()))
     }
     fn call_tool(
         &self,
